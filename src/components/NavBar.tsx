a64367ec50289c9688db5752d4d9387e929348d8
Nicolas Galeano Ramirez <ngaleanor06@gmail.com>
2024-03-23 12:14:28 -0500

    NavBar responsive improved

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
     const handleClick = () => setShowMenu(!showMenu)
 
     const mobile = <>
-        <div className="lg:hidden block absolute top-14 w-full left-0 right-0 bg-black">
+        <div className="md:hidden block absolute top-14 h-10vh w-full left-0 right-0 bg-black">
             <ul className="text-center text-xl p-20">
                 <Link to="/">
                     <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
@@ -37,25 +37,25 @@ export default function NavBar() {
     <nav>
         <div  className="h-10vh flex justify-between z-50 text-white bg-black lg:py-5 px-20 py-4 flex-1">
             <div className="flex items-center flex-1">
-                    <h1 className="font-size-20 font-bold">Nicolás Galeano</h1>
+                    <p className="text-2xl font-bold break-keep max-lg:text-lg">Nicolás Galeano</p>
             </div>
             <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
                 <div className="flex-10">
                     <ul className="flex gap-8 mr-16 text-[18px]">
                         <Link to="/">
-                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 cursor-pointer">Home</li>
+                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 max-lg:text-sm cursor-pointer">Home</li>
                         </Link>
                         <Link to="/projects">
-                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 cursor-pointer">Projects</li>        
+                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 max-lg:text-sm cursor-pointer">Projects</li>        
                         </Link>
                         <Link to="/skills">
-                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 cursor-pointer">Skills</li>
+                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 max-lg:text-sm cursor-pointer">Skills</li>
                         </Link>
                         <Link to="/certifications">
-                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 cursor-pointer">Certifications</li>
+                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 max-lg:text-sm cursor-pointer">Certifications</li>
                         </Link>
                         <Link to="https://github.com/ArmyNicolasG">
-                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 cursor-pointer">GitHub Profile</li>        
+                            <li className="hover:text-teal-300 transition border-b-2 border-white hover:border-teal-300 max-lg:text-sm cursor-pointer">GitHub</li>        
                         </Link>
                     </ul>
                 </div>   
