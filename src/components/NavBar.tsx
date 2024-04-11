eb284e9666b5ad949db20f74e6e95bb81d03c314
Nicolas Galeano Ramirez <ngaleanor06@gmail.com>
2024-04-11 10:09:33 -0500

    Transitions to responsive menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { FaGithub } from "react-icons/fa";
+import { Transition } from '@headlessui/react'
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -27,7 +28,7 @@ export default function NavBar() {
                    <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-green-400"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open menu</span>
@@ -50,6 +51,7 @@ export default function NavBar() {
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          
             <div className="flex items-center justify-between">
               <a href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Nicolás Galeano Portfolio.</span>
@@ -61,36 +63,49 @@ export default function NavBar() {
               </a>
               <button
                 type="button"
-                className="-m-2.5 rounded-md p-2.5 text-green-400"
+                className="-m-2.5 rounded-md p-2.5 text-white"
                 onClick={() => setMobileMenuOpen(false)}
               >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
+            <Transition
+                appear={ true }
+                show={ mobileMenuOpen }
+                enter="transition-all duration-300 ease"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-all duration-300 ease-in-out"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
             <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
+              
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-mx-3 block rounded-lg font-semibold px-3 py-2 leading-7 text-white hover:bg-white hover:text-black"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="py-6">
                     <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 leading-7 text-green-400 hover:bg-white hover:text-black"
+                      href="https://github.com/ArmyNicolasG"
+                      className="-mx-3 text-l block text-4xl rounded-lg px-3 py-2.5 leading-7 text-white hover:text-black hover:bg-white"
                     >
-                      {item.name}
+                      <FaGithub />
                     </a>
-                  ))}
+                  </div>
                 </div>
-                <div className="py-6">
-                  <a
-                    href="https://github.com/ArmyNicolasG"
-                    className="-mx-3 text-l block rounded-lg px-3 py-2.5 font-semibold leading-7 text-green-400 hover:text-black hover:bg-white"
-                  >
-                    GitHub Profile <FaGithub />
-                  </a>
-                </div>
-              </div>
+              
             </div>
+            </Transition>
           </Dialog.Panel>
         </Dialog>
       </header>
