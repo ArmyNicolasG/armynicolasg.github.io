4aebc5cda8e997adc067ef5d1b0135d2fcbed65b
nicolas <ngaleanor06@gmail.com>
2024-04-25 09:20:59 -0500

    some corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Outlet } from 'react-router-dom'
 import WelcomeMessage from './routes/Home/components/WelcomeMessage.tsx'
 import Skills from './components/LanguageCarousel'
 import './App.css'
+// import { useState } from 'react';
 
 function App() {
+
+  // const [isLoaded, setIsLoaded] = useState(false)
+
   return (
      <>
       <div className="mt-40 lg:mt-0 text-white h-screen">
--- src/components/LanguageElement.tsx
+++ src/components/LanguageElement.tsx
@@ -1,10 +1,10 @@
-export default function LanguageElement ({ src, info, size }) {
+export default function LanguageElement ({src, info, size} : { src: string, info: string, size: any  }) {
 
   return ( 
       <a className={`rounded-full h-${ size.bubble } w-auto`} href={ info }>
           <img
               src={ src }
-              className={$`h-${ size.img } w-auto`}
+              className={`h-${ size.img } w-auto`}
           />
       </a>
   )
